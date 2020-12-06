dbd66476f3a106b624970fe75e48078ac251f135
jasii <josephsepe@gmail.com>
2020-12-05 18:29:04 -0800

    first commit

---
+++ BITSEFClasses/Models/Barrel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BITSEFClasses.Models
+{
+    public partial class Barrel
+    {
+        public int BrewContainerId { get; set; }
+        public string Treatment { get; set; }
+
+        public virtual BrewContainer BrewContainer { get; set; }
+    }
+}
